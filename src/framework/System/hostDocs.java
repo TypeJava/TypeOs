900fb7f64848816eabe55101b8c7598c4cbb5707
Levi <86807700+BioShot@users.noreply.github.com>
2022-08-15 17:39:18 -0400

    Add files via upload

---
+++ src/framework/System/hostDocs.java
@@ -0,0 +1,24 @@
+//Made By Bio Shot \\
+package framework.System;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class hostDocs {
+    public static void print(String output) {
+        System.out.println(output);
+    }
+
+    public static void createHost() throws IOException {
+        Process p = Runtime.getRuntime().exec("\"TypeOs/src/framework/System/exec.bat\""); //amogu
+
+    }
+
+}
